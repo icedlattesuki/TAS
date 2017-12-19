5c655a0d80909c12c9b754971a9e619c8083dcfc
LinSHP <lsp7572@gmail.com>
2017-12-19 16:20:23 +0800

    tas.sql增加homework

--- tas.sql
+++ tas.sql
@@ -1,120 +1,120 @@
 create database tas;
 use tas;
 create table student(
-    id char(10) primary key,
-    password varchar(30),
-    name varchar(15),
-    college varchar(20),
-    email varchar(30),
-    image_position varchar(60),
-    signature varchar(30),
-    profile varchar(100),
-    major varchar(20),
-    grade int(1),
-    class_number varchar(10),
-    phone_number varchar(15)
+  id char(10) primary key,
+  password varchar(30),
+  name varchar(15),
+  college varchar(20),
+  email varchar(30),
+  image_position varchar(60),
+  signature varchar(30),
+  profile varchar(100),
+  major varchar(20),
+  grade int(1),
+  class_number varchar(10),
+  phone_number varchar(15)
 );
 create table teacher(
-    id char(6) primary key,
-    password varchar(30),
-    name varchar(15),
-    college varchar(20),
-    email varchar(30),
-    image_position varchar(60),
-    signature varchar(30),
-    profile varchar(100),
-    title varchar(10),
-    phone_number varchar(15)
+  id char(6) primary key,
+  password varchar(30),
+  name varchar(15),
+  college varchar(20),
+  email varchar(30),
+  image_position varchar(60),
+  signature varchar(30),
+  profile varchar(100),
+  title varchar(10),
+  phone_number varchar(15)
 );
 create table course(
-    id varchar(10),
-    name varchar(20),
-    credit decimal(2,1),
-    college varchar(20),
-    semester varchar(15),
-    time varchar(15),
-    place varchar(20),
-    introduction varchar(100),
-    like_number int(4),
-    primary key (id, semester, time, place)
+  id varchar(10),
+  name varchar(20),
+  credit decimal(2,1),
+  college varchar(20),
+  semester varchar(15),
+  time varchar(15),
+  place varchar(20),
+  introduction varchar(100),
+  like_number int(4),
+  primary key (id, semester, time, place)
 );
 create table take(
-    student_id char(10),
-    course_id varchar(10),
-    semester varchar(15),
-    time varchar(15),
-    place varchar(20),
-    primary key (student_id, course_id),
-    foreign key (student_id) references student(id),
-    foreign key (course_id, semester, time, place) references course(id, semester, time, place)
-);  
+  student_id char(10),
+  course_id varchar(10),
+  semester varchar(15),
+  time varchar(15),
+  place varchar(20),
+  primary key (student_id, course_id),
+  foreign key (student_id) references student(id),
+  foreign key (course_id, semester, time, place) references course(id, semester, time, place)
+);
 create table teach(
-    teacher_id char(6),
-    course_id varchar(10),
-    semester varchar(15),
-    time varchar(15),
-    place varchar(20),
-    primary key (teacher_id, course_id),
-    foreign key (teacher_id) references teacher(id),
-    foreign key (course_id, semester, time, place) references course(id, semester, time, place)
+  teacher_id char(6),
+  course_id varchar(10),
+  semester varchar(15),
+  time varchar(15),
+  place varchar(20),
+  primary key (teacher_id, course_id),
+  foreign key (teacher_id) references teacher(id),
+  foreign key (course_id, semester, time, place) references course(id, semester, time, place)
 );
 create table file(
-    id int(10) primary key auto_increment,
-    name varchar(100),
-    location varchar(200),
-    size int(10),
-    date datetime,
-    course_id varchar(10),
-    semester varchar(15),
-    time varchar(15),
-    place varchar(20),
-    user_id varchar(10),
-    foreign key (course_id, semester, time, place) references course(id, semester, time, place)
+  id int(10) primary key auto_increment,
+  name varchar(100),
+  location varchar(200),
+  size int(10),
+  date datetime,
+  course_id varchar(10),
+  semester varchar(15),
+  time varchar(15),
+  place varchar(20),
+  user_id varchar(10),
+  foreign key (course_id, semester, time, place) references course(id, semester, time, place)
 );
 create table material (
-    file_id int(10),
-    foreign key (file_id) references file(id)
+  file_id int(10),
+  foreign key (file_id) references file(id)
 );
 create table video (
-    file_id int(10),
-    title varchar(50),
-    profile varchar(300),
-    foreign key (file_id) references file(id)
+  file_id int(10),
+  title varchar(50),
+  profile varchar(300),
+  foreign key (file_id) references file(id)
 );
 create table announcement(
-    title varchar(30),
-    content varchar(300),
-    date datetime,
-    course_id varchar(10),
-    semester varchar(15),
-    time varchar(15),
-    place varchar(20),
-    foreign key (course_id, semester, time, place) references course(id, semester, time, place)
+  title varchar(30),
+  content varchar(300),
+  date datetime,
+  course_id varchar(10),
+  semester varchar(15),
+  time varchar(15),
+  place varchar(20),
+  foreign key (course_id, semester, time, place) references course(id, semester, time, place)
 );
 create table notice(
-    id int(10) primary key auto_increment,
-    user_id varchar(10),
-    course_id varchar(10),
-    semester varchar(15),
-    time varchar(15),
-    place varchar(20),
-    message varchar(300),
-    type int(1),
-    date datetime,
-    foreign key (course_id, semester, time, place) references course(id, semester, time, place)
+  id int(10) primary key auto_increment,
+  user_id varchar(10),
+  course_id varchar(10),
+  semester varchar(15),
+  time varchar(15),
+  place varchar(20),
+  message varchar(300),
+  type int(1),
+  date datetime,
+  foreign key (course_id, semester, time, place) references course(id, semester, time, place)
 );
 create table comment(
-    id int(10) primary key auto_increment,
-    user_id varchar(10),
-    user_name varchar(15),
-    user_image_position varchar(60),
-    course_id varchar(10),
-    semester varchar(15),
-    time varchar(15),
-    place varchar(20),
-    content varchar(300),
-    date datetime,
-    foreign key (course_id, semester, time, place) references course(id, semester, time, place)
+  id int(10) primary key auto_increment,
+  user_id varchar(10),
+  user_name varchar(15),
+  user_image_position varchar(60),
+  course_id varchar(10),
+  semester varchar(15),
+  time varchar(15),
+  place varchar(20),
+  content varchar(300),
+  date datetime,
+  foreign key (course_id, semester, time, place) references course(id, semester, time, place)
 );
 
 CREATE TABLE homework(
@@ -139,9 +139,9 @@ insert into student values("3150103200", "123456", "张智彬", "计算机科学
 insert into student values("3150101000", "123456", "张耀心", "计算机科学与技术学院", "", "", "", "", "软件工程", "3", "1502", "");
 insert into student values("3150101300", "123456", "吴佳豪", "计算机科学与技术学院", "", "", "", "", "软件工程", "3", "1503", "");
 
-insert into teacher values("300400", "123456", "邢卫", "计算机科学与技术学院", "", "", "", "", "教授");
-insert into teacher values("300500", "123456", "刘玉生", "计算机科学与技术学院", "", "", "", "", "教授");
-insert into teacher values("300600", "123456", "林海", "计算机科学与技术学院", "", "", "", "", "教授");
+insert into teacher values("300400", "123456", "邢卫", "计算机科学与技术学院", "", "", "", "", "教授", "");
+insert into teacher values("300500", "123456", "刘玉生", "计算机科学与技术学院", "", "", "", "", "教授", "");
+insert into teacher values("300600", "123456", "林海", "计算机科学与技术学院", "", "", "", "", "教授", "");
 
 insert into course values("Ex100", "数据结构与算法分析", "2.5", "计算机科学与技术学院", "春学期", "周一3、4", "紫金港-东1-104", "", "0");
 insert into course values("Ex100", "数据结构与算法分析", "2.5", "计算机科学与技术学院", "春学期", "周一3、4", "紫金港-东1-204", "", "0");
@@ -154,6 +154,10 @@ insert into teach values("300400", "Ex100", "春学期", "周一3、4", "紫金�
 insert into teach values("300500", "Ex100", "春学期", "周一3、4", "紫金港-东1-204");
 insert into teach values("300600", "Ex200", "春学期", "周五3、4、5", "玉泉-教7-103");
 
-
+INSERT INTO tas.homework (id, title, content, create_date, ddl_date, score, attachment, course_id, semester, time, place) VALUES (1, '123change', '123change', '2017-12-16', '2017-12-19', 100, '', 'Ex100', '春学期', '周一3、4', '紫金港-东1-104');
+INSERT INTO tas.homework (id, title, content, create_date, ddl_date, score, attachment, course_id, semester, time, place) VALUES (2, '4774', '黄海波 v 好', '2017-12-17', '2017-12-19', 100, '', 'Ex100', '春学期', '周一3、4', '紫金港-东1-104');
+INSERT INTO tas.homework (id, title, content, create_date, ddl_date, score, attachment, course_id, semester, time, place) VALUES (3, 'hbybqwe', 'hbqwyeyy', '2017-12-17', '2017-12-27', 100, '', 'Ex100', '春学期', '周一3、4', '紫金港-东1-104');
+INSERT INTO tas.homework (id, title, content, create_date, ddl_date, score, attachment, course_id, semester, time, place) VALUES (4, 'test4', 'test4', '2017-12-17', '2017-12-19', 100, '', 'Ex100', '春学期', '周一3、4', '紫金港-东1-104');
+INSERT INTO tas.homework (id, title, content, create_date, ddl_date, score, attachment, course_id, semester, time, place) VALUES (5, 'test5', 'test5', '2017-12-17', '2017-12-19', 100, '', 'Ex100', '春学期', '周一3、4', '紫金港-东1-104');
 
 
